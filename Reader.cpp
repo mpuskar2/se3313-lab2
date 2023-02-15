450c586d97924a186b294abb2762d7663570bc4d
mpuskar2 <marko.p1211@gmail.com>
2023-02-15 01:27:02 -0500

    Completed no semaphore version

--- Reader.cpp
+++ Reader.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include "SharedObject.h"
+
+struct MyShared{
+    int threadNumber;
+    int reportID;
+    int delay;
+};
 
 int main(void)
 {
-	std::cout << "I am a reader" << std::endl;
+    Shared<MyShared> sharedMemory("sharedMemory");
+
+    int count = 0;
+    while (count < 30){
+        std::cout<<"Thread Number: " << sharedMemory->threadNumber << ", Report ID: " << sharedMemory->reportID << ", Delay: " << sharedMemory->delay <<"\n";
+        count++;
+        sleep(1);
+    }
 }
--- Writer.cpp
+++ Writer.cpp
@@ -1,59 +1,82 @@
 #include <iostream>
+#include "thread.h"
+#include "SharedObject.h"
+#include <thread>
+#include <stack>
 
-/*
 struct MyShared{
-	...;
-};*/
+    int threadNumber;
+    int reportID;
+    int delay;
+};
+
+class WriterThread : public Thread{
+public:
+    int threadNum;
+    int reportID;
+    int delay;
+    bool flag = false;
+
+    WriterThread(int threadNum, int delayTime):Thread(delayTime * 1000){
+        this->threadNum = threadNum;
+        this->delay = delayTime;
+    }
+
+    virtual long ThreadMain(void) override{
+        //declare shared memory var so this thread can access it
+        Shared<MyShared> sharedMemory ("sharedMemory");
+
+        while(true)
+        {
+            //write to shared memory
+            this->reportID++;
+            sleep(delay);
+
+            sharedMemory-> threadNumber = threadNum;
+            sharedMemory-> reportID = reportID;
+            sharedMemory-> delay = delay;
+
+            if(flag){//Exit loop to end the thread
+                break;
+            }
+        }
+        return 1;
+    }
+};
 
 int main(void)
 {
-	std::cout << "I am a Writer" << std::endl;
-	
-	////////////////////////////////////////////////////////////////////////
-	// This is a possible starting point for using threads and shared memory. 
-	// You do not have to start with this
-	////////////////////////////////////////////////////////////////////////
-	/*...
-	Shared<MyShared> shared("sharedMemory", true); //This is the owner of sharedMamory
-	...
+    std::string userInput;
+    std::string delayTimeStr;
+    int threadNumber = 0;
+    std::stack<WriterThread*> threadStack;
+
+	Shared<MyShared> shared("sharedMemory", true); //This is the owner of sharedMemory
+    WriterThread * thread1;
+
 	while(true){
-		...
-		//create thread using user input
-		thread1 = new WriterThread(xyz); //add arguments as needed
-		...
+        std::cout << "Would you like to create a writer thread?\n";
+        getline(std::cin, userInput);
+
+        if (userInput == "n") {
+            break;
+        }
+        else {
+            std::cout << "What is the delay time for this thread?\n";
+            //std::cin >> delayTime;
+            getline(std::cin, delayTimeStr);
+            int delayTime = stoi(delayTimeStr);
+            threadNumber++;
+            //create thread using user input
+            thread1 = new WriterThread(threadNumber, delayTime);
+            threadStack.push(thread1);
+        }
 	}
-	//example for one thread thread1
-	thread1->flag= true;
-	delete thread1;
-	*/
+
+    for(int i = 0; i < threadStack.size(); i++){
+        thread1 = threadStack.top();
+        thread1->flag = true;
+        delete thread1;
+        threadStack.pop();
+    }
 }
-
-
-////////////////////////////////////////////////////////////////////////
-// This is a possible starting point for using threads and shared memory. 
-// You do not have to start with this
-////////////////////////////////////////////////////////////////////////
-/*class WriterThread : public Thread{
-	public:
-		int 	threadNum;
-		bool	flag;
-		
-		WriterThread(int in):Thread(8*1000){
-			this->threadNum = in; //or whatever you want/need here
-		}
-
-		virtual long ThreadMain(void) override{
-			...
-			//declare shared memory var so this thread can access it
-			Shared<MyShared> sharedMemory ("sharedMemory");
-			while(true)
-			{
-				//write to shared memory
-				...  
-				if(flag){//Exit loop to end the thread
-					break;
-				}
-			}
-		}
-};
-*/
