02fe94d1b9ead2e3c97105e85cf02b65c4b6a89a
mpuskar2 <marko.p1211@gmail.com>
2023-02-28 23:44:49 -0500

    Completed semaphore version

--- Reader.cpp
+++ Reader.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include "SharedObject.h"
+#include "Semaphore.h"
+
+struct MyShared{
+    int threadNumber;
+    int reportID;
+    int totalTime;
+};
 
 int main(void)
 {
-	std::cout << "I am a reader" << std::endl;
+    Semaphore writer("writer", 1, false);
+    Semaphore reader("reader", 1, false);
+
+    Shared<MyShared> sharedMemory("sharedMemory");
+
+    int count = 0;
+    while (count < 30){
+        reader.Wait();
+        std::cout<<"Thread Number: " << sharedMemory->threadNumber << ", Report ID: " << sharedMemory->reportID << ", Total Time: " << sharedMemory->totalTime <<"\n";
+        count++;
+        writer.Signal();
+    }
 }
